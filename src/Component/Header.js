61aef33174e5160d22c005c4f7dda51884ff6666
saramccombs <theSaraMcCombs@gmail.com>
2020-06-04 00:31:30 -0400

    Update nav menu

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,11 +1,10 @@
 import React, { Component} from 'react';
 import Logo from '../Assets/Images/logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Home_1 from '../Assets/Images/home/img-1.jpg';
-import Home_2 from '../Assets/Images/home/img-2.jpg';
-import Home_4 from '../Assets/Images/home/img-4.jpg';
-import Home_5 from '../Assets/Images/home/img-5.jpg';
-import Home_6 from '../Assets/Images/home/img-6.jpg';
+import Home_1 from '../Assets/Images/home/menu.jpg';
+import Home_2 from '../Assets/Images/home/menu-2.jpg';
+import Home_3 from '../Assets/Images/home/menu-3.jpg';
+import Home_4 from '../Assets/Images/home/menu-4.jpg';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
@@ -41,28 +40,23 @@ class Header extends Component {
                             </button>
                             <ul className="nav navbar-nav">
                                 <li className="home">
-                                    <a href="#home">
+                                    <a href="../../public/index.html" onClick={() => this.setState({isOpen: false})}>
                                         <img src={Home_1} alt=""/>
                                     </a>
                                 </li>
                                 <li className="about">
-                                    <a href="#about">
+                                    <a href="#about" onClick={() => this.setState({isOpen: false})}>
                                         <img src={Home_2} alt=""/>
                                     </a>
                                 </li>
-                                <li className="portfolio">
-                                    <a href="#portfolio">
-                                        <img src={Home_4} alt=""/>
-                                    </a>
-                                </li>
                                 <li className="blog">
-                                    <a href="#blog">
-                                        <img src={Home_5} alt=""/>
+                                    <a href="#blog" onClick={() => this.setState({isOpen: false})}>
+                                        <img src={Home_3} alt=""/>
                                     </a>
                                 </li>
                                 <li className="contact">
-                                    <a href="#contact">
-                                        <img src={Home_6} alt=""/>
+                                    <a href="#contact" onClick={() => this.setState({isOpen: false})}>
+                                        <img src={Home_4} alt=""/>
                                     </a>
                                 </li>
                             </ul>
