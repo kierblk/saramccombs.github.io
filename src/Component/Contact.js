8763e5d0b193fe147de90b7598f8090465a2c184
saramccombs <theSaraMcCombs@gmail.com>
2020-06-03 21:53:43 -0400

    Updated contact component, added form action to formspree.

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -11,42 +11,24 @@ class Contact extends Component {
                     <div className="row">
                         <div className="col-lg-5 col-md-5 col-sm-5">
                             <div className="contact-page-item">
-                                <h2>My Contacts</h2>
-                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-                                <div className="adress">
-                                    <h3>Address</h3>
-                                    <span>245 King Street, Touterie Victoria 8520 Australia</span>
-                                </div>
-                                <div className="phone">
-                                    <h3>Phone</h3>
-                                    <span>0-123-456-7890</span>
-                                    <span>0-123-456-7890</span>
-                                </div>
-                                <div className="email">
-                                    <h3>Email</h3>
-                                    <span>sample@gmail.com</span>
-                                </div>
+                                <h2>Contact Me</h2>
+                                <p>Interested in what I'm currently working on? Have a great resource you think I'd like? Want to tell me how cute my dogs are?</p>
+                                <p>
+                                Feel free to drop me a line here or connect with me using one of the links in the <a href="#about">about me</a> section.</p>
                             </div>
                         </div>
                         <div className="col-lg-7 col-md-7 col-sm-7">
                             <div className="contact-area contact-area-2 contact-area-3">
-                                <h2>Quick Contact Form</h2>
                                 <div className="contact-form">
-                                    <form method="post" className="contact-validation-active" id="contact-form">
+                                    <form method="post" action="https://formspree.io/thesaramccombs@gmail.com" className="contact-validation-active" id="contact-form">
                                         <div className="half-col">
                                             <input type="text" name="name" id="name" className="form-control" placeholder="Your Name" />
                                         </div>
                                         <div className="half-col">
                                             <input type="email" name="email" id="email" className="form-control" placeholder="Your Email" />
                                         </div>
-                                        <div className="half-col">
-                                            <input type="text" name="phone" id="phone" className="form-control" placeholder="Your Phone"/>
-                                        </div>
-                                        <div className="half-col">
-                                            <input type="text" name="address" id="address" className="form-control" placeholder="Address"/>
-                                        </div>
                                         <div>
-                                            <textarea className="form-control" name="note"  id="note" placeholder="Your Massage..."></textarea>
+                                            <textarea className="form-control" name="note"  id="note" placeholder="Your Message..."></textarea>
                                         </div>
                                         <div className="submit-btn-wrapper">
                                             <button type="submit" className="theme-btn-s3">Submit</button>
@@ -60,14 +42,6 @@ class Contact extends Component {
                                         </div>
                                     </form>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col col-xs-12">
-                            <div className="contact-map">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57763.58882182253!2d55.38442113562169!3d25.195692423227655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2z4Kam4KeB4Kas4Ka-4KaHIC0g4Kam4KeB4Kas4Ka-4KaHIOCmhuCmruCmv-CmsOCmvuCmpCAtIOCmuOCmguCmr-CngeCmleCnjeCmpCDgpobgprDgpqwg4KaG4Kau4Ka_4Kaw4Ka-4Kak!5e0!3m2!1sbn!2sbd!4v1540725271741" />
-                               
                             </div>
                         </div>
                     </div>
